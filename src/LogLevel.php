1e931eb8589b12c2e04e81752c9a1f3ea4d8e684
amirhf <amirhacking85@gmail.com>
2023-02-09 00:54:45 +0330

    fix: add return value on Log level.

--- src/LogLevel.php
+++ src/LogLevel.php
@@ -9,4 +9,10 @@ enum LogLevel
     case WARN; // Designates potentially harmful situations.
     case ERROR; // Designates error events that might still allow the application to continue running.
     case FATAL; // Designates very severe error events that will presumably lead the application to abort.
+
+    public static function getAllValues(): array
+    {
+        return array_column(LogLevel::cases(), 'value');
+    }
+
 }
